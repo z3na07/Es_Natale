4f5a633787b2dbd600f06b0673cb996bbc60e471
Nicola <150442299+ErPollo07@users.noreply.github.com>
2025-02-05 11:22:23 +0100

    ADD getters and setters in TimezoneManager

--- Code/src/util/TimezoneManager.java
+++ Code/src/util/TimezoneManager.java
@@ -3,4 +3,29 @@ package util;
 public class TimezoneManager {
     public static int timezonePassed;
     public static final int maxTimezone = 3;
+
+    public TimezoneManager(int maxTimezone) {
+        this.maxTimezone = maxTimezone;
+        this.timezonePassed = 0;
+    }
+
+    public boolean isTimeToChange() {
+        return timezonePassed == 3;
+    }
+
+    /* =========================================
+                  TIME ZONE PASSED
+    * ==========================================*/
+
+    public void incrementTimezonePassed() {
+        incrementTimezonePassed(1);
+    }
+
+    public void incrementTimezonePassed(int n) {
+        this.timezonePassed += n;
+    }
+
+    public void setTimezonePassed(int n) {
+        this.timezonePassed = n;
+    }
 }
