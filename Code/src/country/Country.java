a7cd84e3807e8ca19eae83aba7b4c04773ef5536
z3na07 <150442545+z3na07@users.noreply.github.com>
2025-02-12 11:48:44 +0100

    ADD docs

--- Code/src/country/Country.java
+++ Code/src/country/Country.java
@@ -1,35 +1,74 @@
 package country;
 
+/**
+ * Class country that is called when we create a new country, in the class we hava eall the methods we need like the get e sets methods and the constructor
+ */
 public class Country {
     private String name;
     private long population;
     private double goodChildren;
     private String timeZone;
 
+
+    /**
+     * constructor of the class, we call itr when we want to create a new Object country 
+     * 
+     * @param name is te name of the conutry that we are creating
+     * @param population is the total puplation of the country
+    * @param goodChilder is the total in percent of the amount of good children in the country
+    * @param timeZone is the country's time zone
+    */
+
     public Country(String name, long population, double goodChildren, String timeZone) {
-        this.name = name;
         this.population = population;
         this.goodChildren = goodChildren;
         this.timeZone = timeZone;
     }
 
+
+    /**
+    * this methods calculate the population of good chldren in that specific country
+    * 
+    * @return n.longvaue() this methods returns the amount of good children expressed in percent
+    */
     public long getPopulationOfgoodChildren() {
         Number n = population * goodChildren;
         return n.longValue();
     }
 
+    /**
+     * get method of the variable name
+     * 
+     * @return it returns the name of the country that we selected
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * method get of the variable population 
+     * 
+     * @return this method returns the total population ofn the country that we have selected
+     */
     public long getPopulation() {
         return population;
     }
 
+/**
+ * method het of the variabler goodChilder 
+ * 
+ * @return this method returns the total amount of goodl children in a specific country
+ */
     public double getgoodChildren() {
         return goodChildren;
+
     }
 
+    /**
+     * method get of tha variabvle timeZone
+     * 
+     * @return this methods returns the time zone where is located a specific country
+     */
     public String getTimeZone() {
         return timeZone;
     }
--- Code/src/reindeer/Reindeer.java
+++ Code/src/reindeer/Reindeer.java
@@ -1,14 +1,28 @@
 package reindeer;
 
+/**
+ * class reindeer, this class is used when we want to create a new object Reindeer
+ */
 public class Reindeer {
     private String name;
     private boolean isActive;
 
+    /**
+     * contructor of the class reindeer
+     * 
+     * @param name is the name of the reindeer
+     * @param isActive is a boolean variable that says if the reindeer is active/in work or no
+     */
     public Reindeer(String name, boolean isActive) {
         this.name = name;
         this.isActive = isActive;
     }
 
+    /**
+     * Override of the method toString, we do that because we want the method to print in a different way 
+     * 
+     * @return it returns the String that is shown at video with the name and all the booelan variable of the reindeer 
+     */
     @Override
     public String toString() {
         return "Reindeer{" +
@@ -17,10 +31,18 @@ public class Reindeer {
                 '}';
     }
 
+    /**
+     * get method of the variable isActive
+     * 
+     * @return it returns the value of the variable, its true or false
+     */
     public boolean isActive() {
         return isActive;
     }
 
+    /**
+     * set method of the variable isActive, it sets the value that we entered as the current value of the variable
+     */
     public void setActive(boolean active) {
         isActive = active;
     }
